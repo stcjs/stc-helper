0eb4e42c9284a0f92a33dca24d522f5704004e16
lichengyin <welefen@gmail.com>
2016-05-13 14:17:53 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # stc-helper
+
 helper for stc
+
+## How to use
+
+```js
+import {isFile} from 'stc-helper';
+if(isFile('xxx')){
+	
+}
+```
+
+## API
+
+### isStream(stream)
+
+check is stream.
+
+```js
+import {isStream} from 'stc-helper';
+if(isStream(stream){
+	
+}
+```
+
+### isBuffer(buffer)
+
+check is buffer.
+
+### isArray(arr)
+
+check is array.
+
+### isFunction(fn)
+
+check is function.
+
+### isRegExp(regexp)
+
+check is RegExp.
+
+### isExist(filePath)
+
+check file/dir is exist.
+
+### getFiles(dir, prefix = '')
+
+get files from dir.
+
+```js
+import {getFiles} from 'stc-helper';
+
+let files = getFiles('/path/to'); // return is array
+
+```
+
+### promisify(fn, receiver)
+
+make callback function to promise.
+
+```js
+import {promisify} from 'stc-helper';
+import fs from 'fs'
+
+let fn = promisify(fs.readFile, fs);
+fn('/path/to/file', 'utf8').then(content => {
+	
+})
+```
+
+### defer()
+
+get Deferred object.
+
+```js
+import {defer} from 'stc-helper';
+
+let fn => {
+	let deferred = defer();
+	if(xxx){
+		deferred.resolve('xxx');
+	}else{
+		deferred.reject(new Error());
+	}
+	return deferred.promise;
+}
+
+```
+
+### md5(str)
+
+get content md5 value.
+
+### asyncReplace(content, replace, callback)
+
+async content replace
+
+```js
+import {asyncReplace} from 'stc-helper';
+
+let fn = async content => {
+	return asyncReplace(content, /\d+/, (a, b) => {
+		return Promise.resolve(a);
+	});
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Stream from 'stream';
+import util from 'util';
+import crypto from 'crypto';
+import path from 'path';
+import fs from 'fs';
 
 /**
  * is stream
  */
 export function isStream(stream){
-  return !!stream && stream instanceof Stream;
+	return !!stream && stream instanceof Stream;
 }
 
 /**
@@ -17,3 +21,105 @@ export const isBuffer = Buffer.isBuffer;
  */
 export const isArray = Array.isArray;
 
+/**
+ * is function
+ */
+export function isFunction(obj) {
+	return typeof obj === 'function';
+}
+
+/**
+ * is regexp
+ */
+export const isRegExp = util.isRegExp;
+
+/**
+ * check path is exist
+ */
+export function isExist(dir) {
+	dir = path.normalize(dir);
+	if(fs.accessSync){
+		try{
+			fs.accessSync(dir, fs.R_OK);
+			return true;
+		}catch(e){
+			return false;
+		}
+	}
+	return fs.existsSync(dir);
+}
+
+/**
+ * get path files
+ */
+export function getFiles(dir, prefix = ''){
+	if(!isExist(dir)){
+		return [];
+	}
+	let files = fs.readdirSync(dir);
+  let result = [];
+  files.forEach(item => {
+    let stat = fs.statSync(dir + path.sep + item);
+    if (stat.isFile()) {
+      result.push(prefix + item);
+    }else if(stat.isDirectory()){
+      let cFiles = getFiles(dir + path.sep + item, prefix + item + path.sep);
+      result = result.concat(cFiles);
+    }
+  });
+	return result;
+}
+
+/**
+ * make callback to promise
+ */
+export function promisify(fn, receiver) {
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      fn.apply(receiver, [...args, (err, res) => {
+        return err ? reject(err) : resolve(res);
+      }]);
+    });
+  };
+}
+
+/**
+ * defer
+ */
+export function defer(){
+	let deferred = {};
+  deferred.promise = new Promise((resolve, reject) => {
+    deferred.resolve = resolve;
+    deferred.reject = reject;
+  });
+  return deferred;
+}
+
+/**
+ * md5
+ */
+export function md5(str){
+	return crypto.createHash('md5').update(str + '', 'utf8').digest('hex');
+}
+
+/**
+ * async replace content
+ */
+export async function asyncReplace(content = '', replace, callback){
+	let match = content.match(replace);
+	if(!match){
+		return content;
+	}
+	let promises = match.map(args => {
+		return callback(...args);
+	});
+	let data = await Promise.all(promises);
+	let result = '', prevIndex = 0;
+	match.forEach((item, idx) => {
+		let index = content.indexOf(item);
+		result += content.substr(prevIndex, index) + data[idx];
+		prevIndex += item.length;
+	});
+	result += content.substr(prevIndex);
+	return result;
+}
